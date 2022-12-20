c8ab0fa898afdff73558b5b3db9ba3cfb146aab7
Anthony Mercurio <amercurio@coreweave.com>
2022-12-19 19:53:00 -0700

    Add training code

---
+++ src/training/dataset.py
@@ -0,0 +1,104 @@
+import torch
+import argparse
+import numpy as np
+import transformers
+import json
+
+def decode(in_file: str, out_file: str, tokenizer: transformers.AutoTokenizer) -> int:
+    mem = np.memmap(in_file, mode="r", dtype="uint16")
+    tokens = len(mem)
+    with open(out_file, "a") as f:
+        for token in mem:
+            f.write(tokenizer.decode([token]))
+    return tokens
+
+def encode(in_file: str, out_file: str, tokenizer: transformers.AutoTokenizer) -> int:
+    with open(in_file, "r", encoding="utf-8") as f:
+        text = f.read()
+    tokens = tokenizer.encode(text)
+    with open(out_file, "wb") as f:
+        for token in tokens:
+            f.write(np.uint16(token))
+    return len(tokens)
+
+class TokenizedDataset(torch.utils.data.Dataset):
+    def __init__(self, in_file: str, chunks: int, tokenizer: transformers.AutoTokenizer):
+        self.in_file = in_file
+        self.chunks = chunks
+        self.tokenizer = tokenizer
+        self.mem = np.memmap(in_file, mode="r", dtype="uint16")
+
+    def __len__(self):
+        return len(self.mem) // self.chunks
+    
+    def __getitem__(self, idx):
+        return torch.tensor(self.mem[idx*self.chunks:(idx+1)*self.chunks])
+
+class FeedbackDataset(torch.utils.data.Dataset):
+    def __init__(self, feedback_file: str, tokenizer: transformers.AutoTokenizer, max_length: int = 512):
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+        self.feedback_file = feedback_file
+        
+        with open(feedback_file) as f:
+            self.feedback = [json.loads(line) for line in f]
+
+    def __len__(self):
+        return len(self.feedback)
+
+    def __getitem__(self, idx):
+        feedback = self.feedback[idx]
+        feedback_input = '\n'.join(feedback["input"].split("\n")[-2:])
+        feedback_str = f'{feedback_input} {feedback["output"].lstrip().rstrip()}'
+        seq = self.tokenizer(
+            feedback_str,
+            padding="max_length",
+            truncation=True,
+            return_tensors="pt"
+            )
+        reward = torch.tensor([feedback["reward"]]).unsqueeze(0)
+        return seq, reward
+
+# sft file example
+# {
+#     "input": "Anonymous: Hi, how are you?\nGPT:",
+#     "output": " I'm good, how are you?\n",
+#     "reward": 0.0
+# }
+class SFTDataset(torch.utils.data.Dataset):
+    def __init__(self, sft_file: str, tokenizer: transformers.AutoTokenizer, max_length: int = 512):
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+        self.sft_file = sft_file
+        
+        with open(sft_file) as f:
+            self.sft = [json.loads(line) for line in f]
+        
+        # iterate over sft, removing any that have a reward of 0
+        self.sft = [sft for sft in self.sft if sft["reward"] != 0.0]
+
+    def __len__(self):
+        return len(self.sft)
+    
+    def __getitem__(self, idx):
+        sft = self.sft[idx]
+        sft_input_tokens = self.tokenizer(sft["input"], return_tensors="pt").input_ids
+        sft_output_tokens = self.tokenizer(sft["output"], return_tensors="pt").input_ids
+        input_ids = torch.cat([sft_input_tokens, sft_output_tokens], dim=-1)
+        start_positions = torch.tensor([len(sft_input_tokens[0])])
+        end_positions = torch.tensor([len(sft_input_tokens[0]) + len(sft_output_tokens[0]) - 1])
+        return {
+            "input_ids": input_ids,
+            "start_positions": start_positions,
+            "end_positions": end_positions,
+        }
+        
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Dataset Creator')
+    parser.add_argument('--in_file', type=str, help='input file to use', required=True)
+    parser.add_argument('--out_file', type=str, help='output file to use', required=True)
+    parser.add_argument('--model', type=str, help='model tokenizer to use', required=True)
+    args = parser.parse_args()
+
+    encode(args.in_file, args.out_file, transformers.AutoTokenizer.from_pretrained(args.model))
--- src/training/sft.py
+++ src/training/sft.py
@@ -1,4 +1,12 @@
+import os
 import torch
+import accelerate
+import tqdm
+import time
+import argparse
+import wandb
+
+from dataset import TokenizedDataset, FeedbackDataset, SFTDataset
 
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from transformers.modeling_outputs import CausalLMOutput
@@ -55,24 +63,172 @@ def sft_forward(
         attentions=outputs.attentions,
     )
 
-if __name__ == '__main__':
-    # Load model and tokenizer
-    model = AutoModelForCausalLM.from_pretrained('distilgpt2')
-    tokenizer = AutoTokenizer.from_pretrained('distilgpt2')
+class SFT_Trainer:
+    def __init__(
+        self,
+        accelerator: accelerate.Accelerator,
+        model: AutoModelForCausalLM,
+        tokenizer: AutoTokenizer,
+        train_dataloader: torch.utils.data.DataLoader,
+        optimizer: torch.optim.Optimizer,
+        weight_dtype: torch.dtype,
+        args: argparse.Namespace,
+    ) -> None:
+        self.accelerator = accelerator
+        self.model = model
+        self.tokenizer = tokenizer
+        self.train_dataloader = train_dataloader
+        self.optimizer = optimizer
+        self.weight_dtype = weight_dtype
+        self.args = args
 
-    # Add supervised finetuning forward method to model
-    model.forward = sft_forward.__get__(model)
+        if accelerator.is_main_process:
+            self.progress_bar = tqdm.tqdm(
+                total=self.args.epochs*len(train_dataloader),
+                desc="Total Steps",
+                leave=False,
+            )
 
-    # Create input tensors
-    question = 'What is the capital of France?'
-    answer = 'The capital of France is Paris.'
-    question_tokens = tokenizer.encode(question, return_tensors='pt')
-    answer_tokens = tokenizer.encode(answer, return_tensors='pt')
-    input_ids = torch.cat([question_tokens, answer_tokens], dim=-1)
+            self.run = wandb.init(
+                project="convogpt-sftlm",
+                name=f'{self.args.model}-{self.args.epochs}-{self.args.batch_size}-{self.args.learning_rate}--{int(time.time())}',
+                config=self.args,
+            )
+
+            self.global_step = 0
     
-    start_positions = torch.tensor([len(question_tokens[0])])
-    end_positions = torch.tensor([len(question_tokens[0]) + len(answer_tokens[0]) - 1])
+    def save_model(self) -> None:
+        self.accelerator.wait_for_everyone()
+        path = f'{self.args.output_dir}/{self.run.name}'
+        os.makedirs(path, exist_ok=True)
+        model = self.accelerator.unwrap_model(self.model)
+        model.save_pretrained(path)
 
-    # Compute loss
-    loss = model(input_ids, start_positions=start_positions, end_positions=end_positions).loss
-    print(loss)
+    def step(self, batch: dict) -> None:
+        with self.accelerator.accumulate(self.model):
+            input_ids = batch['input_ids']
+            attention_mask = batch['attention_mask']
+            start_positions = batch['start_positions']
+            end_positions = batch['end_positions']
+
+            outputs = sft_forward(
+                self.model,
+                input_ids=input_ids,
+                attention_mask=attention_mask,
+                start_positions=start_positions,
+                end_positions=end_positions,
+            )
+
+            loss = outputs.loss
+            self.accelerator.backward(loss)
+            if self.accelerator.sync_gradients:
+                self.accelerator.clip_grad_norm_(self.model.parameters(), 1.0)
+            self.optimizer.step()
+            self.optimizer.zero_grad()
+        
+        return {
+            "train/loss": loss.detach().item(),
+        }
+    
+    def train(self) -> None:
+        self.model.train()
+        for epoch in range(self.args.epochs):
+            for _, batch in enumerate(self.train_dataloader):
+                step_start = time.perf_counter()
+
+                metrics = self.step(batch)
+
+                step_end = time.perf_counter()
+
+                if self.accelerator.is_main_process:
+                    rank_samples_per_second = self.args.batch_size / (step_end - step_start)
+                    world_samples_per_second = rank_samples_per_second * self.accelerator.num_processes
+
+                    metrics.update({
+                        "perf/rank_samples_per_second": rank_samples_per_second,
+                        "perf/world_samples_per_second": world_samples_per_second,
+                        "train/epoch": epoch,
+                        "train/step": self.global_step,
+                        "train/samples_seen": self.global_step * self.args.batch_size,
+                    })
+
+                    self.global_step += 1
+
+                    self.progress_bar.update(1)
+                    self.progress_bar.set_postfix(**metrics)
+
+                    self.run.log(metrics, step=self.global_step)
+
+                    if self.global_step % self.args.save_steps == 0:
+                        self.save_model()
+        self.accelerator.wait_for_everyone()
+        self.save_model()
+
+def main() -> None:
+
+    parser = argparse.ArgumentParser(description="Supervised GPT finetuning")
+    parser.add_argument("--model", type=str, default="hakurei/gpt-j-random-tinier", help="Model name")
+    parser.add_argument("--dataset", type=str, default="train.jsonl", help="Training file")
+    parser.add_argument("--output_dir", type=str, default="output", help="Output directory")
+    parser.add_argument("--epochs", type=int, default=1, help="Number of epochs")
+    parser.add_argument("--batch_size", type=int, default=1, help="Batch size")
+    parser.add_argument("--save_steps", type=int, default=1000, help="Save model every x steps")
+    parser.add_argument("--learning_rate", type=float, default=1e-4, help="Learning rate")
+    args = parser.parse_args()
+
+    accelerator = accelerate.Accelerator()
+    accelerate.utils.set_seed(42)
+
+    tokenizer = AutoTokenizer.from_pretrained(args.model)
+    tokenizer.pad_token = tokenizer.eos_token
+
+    def collate_fn(batches):
+        input_ids = [
+            batch["input_ids"].squeeze(0) for batch in batches
+        ]
+        padded_tokens = tokenizer.pad(
+            {"input_ids": input_ids}, return_tensors="pt", padding=True
+        )
+        start_positions = torch.stack(
+            [batch["start_positions"] for batch in batches]
+        )
+        end_positions = torch.stack(
+            [batch["end_positions"] for batch in batches]
+        )
+        return {
+            "input_ids": padded_tokens["input_ids"],
+            "attention_mask": padded_tokens["attention_mask"],
+            "start_positions": start_positions,
+            "end_positions": end_positions,
+        }
+    
+    train_dataset = SFTDataset(args.dataset, tokenizer)
+
+    train_dataloader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=args.batch_size,
+        shuffle=True,
+        collate_fn=collate_fn,
+    )
+
+    model = AutoModelForCausalLM.from_pretrained(args.model)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=args.learning_rate)
+
+    model, optimizer, train_dataloader = accelerator.prepare(
+        model, optimizer, train_dataloader
+    )
+
+    trainer = SFT_Trainer(
+        accelerator=accelerator,
+        model=model,
+        tokenizer=tokenizer,
+        train_dataloader=train_dataloader,
+        optimizer=optimizer,
+        weight_dtype=None,
+        args=args,
+    )
+
+    trainer.train()
+
+if __name__ == '__main__':
+    main()
